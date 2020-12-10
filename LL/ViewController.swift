43bd77da92188b24601c1a1b54e268d81e45ad13
Jose Rivera <jose.rivera@dormakaba.com>
2020-12-10 13:38:29 +0100

    ViewModel - Update remove node ViewController - Added UITableView Delegate

--- LL/ViewController.swift
+++ LL/ViewController.swift
@@ -10,14 +10,51 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var mainTableView: UITableView!
-    
+    var viewModel: ViewModel?
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
 }
 
+// MARK: - UITableView
 
+extension ViewController: UITableViewDelegate {
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+    
+    func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        return true
+    }
+    
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if (editingStyle == .delete) {
+            self.viewModel?.remove(index: indexPath.row)
+        }
+    }
+    
+}
+
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.viewModel?.getList().count ?? 0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let node = self.viewModel?.getList()[indexPath.row]
+        var cell = tableView.dequeueReusableCell(withIdentifier: "Cell")
+
+        if (cell == nil) {
+            cell = UITableViewCell(style: UITableViewCell.CellStyle.subtitle, reuseIdentifier: "Cell")
+        }
+        cell?.textLabel?.text = node
+        return cell!    
+    }
+}
+
+// MARK: - Output
 
--- LL/ViewModel.swift
+++ LL/ViewModel.swift
@@ -9,12 +9,13 @@ import Foundation
 
 protocol ViewModelInput {
     func addValue(value: String)
-    func remove(node: Node<String>)    
+    func remove(index: Int)
+    func getList() -> [String]
 }
 
 protocol ViewModelOutput {
     func showMessage(title: String, body: String)
-    func listUpdated()
+    func listUpdated() 
 }
 
 class ViewModel {
@@ -27,17 +28,32 @@ class ViewModel {
         self.linkedList = linkedList
         list = linkedList.getValues()
     }
+    
+    private func updateList() {
+        list = linkedList.getValues()
+        self.output?.listUpdated()
+    }
 }
 
 extension ViewModel: ViewModelInput {
-    func addValue(value: String) {
-        linkedList.append(value: value)
-        self.output?.listUpdated()
+    func getList() -> [String] {
+        return list
     }
     
-    func remove(node: Node<String>) {
-        let removedNode = linkedList.remove(node: node)
-        self.output?.showMessage(title: "Information", body: "Node with value  \(removedNode) has been removed correctly")
-        self.output?.listUpdated()
+    func addValue(value: String) {
+        linkedList.append(value: value)
+        self.updateList()
+    }
+    
+    func remove(index: Int) {
+        let node = linkedList.nodeAt(index: index)
+        if let node = node {
+            let removedNode = linkedList.remove(node: node)
+            self.output?.showMessage(title: "Information", body: "Node with value  \(removedNode) has been removed correctly")
+            self.updateList()
+        } else {
+            self.output?.showMessage(title: "Error", body: "Error deleting node.")
+        }
+        
     }
 }
